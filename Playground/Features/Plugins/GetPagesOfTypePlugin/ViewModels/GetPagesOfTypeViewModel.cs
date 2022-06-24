6ebb04d2ad18397dbdb1371cb7285309b9ee5f6e
Micael Ståhl <46671686+MicaelStahl@users.noreply.github.com>
2022-06-24 02:02:48 +0200

    Created a plugin to ease the possibility of tracking what page types are in use and not.

---
+++ Playground/Features/Plugins/GetPagesOfTypePlugin/ViewModels/GetPagesOfTypeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Playground.Features.Plugins.GetPagesOfTypePlugin.Models;
+
+namespace Playground.Features.Plugins.GetPagesOfTypePlugin.ViewModels
+{
+    public class GetPagesOfTypeViewModel
+    {
+        public IReadOnlyList<Page> Pages { get; set; }
+
+        public string PageType { get; set; }
+
+        public IReadOnlyList<PageType> PageTypes { get; set; }
+    }
+}
